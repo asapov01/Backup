7f41e4d9e0a97f72a133e50721001c03c38d6e46
Asapov <137000528+asapov01@users.noreply.github.com>
2024-07-24 15:26:05 +0300

    Update server-upgrade.sh

--- server-upgrade.sh
+++ server-upgrade.sh
@@ -30,15 +30,14 @@ screen -dmS upgrade bash -c "
   pip3 --version
 
   # Встановлення Docker
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo \"deb [arch=\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-  docker version
+  sudo apt-get install -y docker-ce=5:26.1.4-1~ubuntu.20.04~focal docker-ce-cli=5:26.1.4-1~ubuntu.20.04~focal containerd.io
+  export PATH=/usr/local/bin:$PATH
+  docker -v
 
   # Встановлення Docker Compose
-  sudo apt-get install -y docker-compose
-  docker-compose --version
+  sudo curl -L "https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+  sudo chmod +x /usr/local/bin/docker-compose
+  docker-compose -v
 
   #Rust
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
